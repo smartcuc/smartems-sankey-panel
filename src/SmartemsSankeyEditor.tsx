eca6dd1e57e22d224c1e010a8adabf9cd608c7e2
smartcuc <cuc@bc-wes.de>
2020-01-15 01:32:42 +0100

    Init

--- src/SmartemsSankeyEditor.tsx
+++ src/SmartemsSankeyEditor.tsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+
 import {
   FormField,
   ThresholdsEditor,
@@ -14,7 +15,7 @@ import {
 import { 
   PanelEditorProps,
   FieldDisplayOptions,
-  ThresholdsConfig,
+  Threshold,
   ValueMapping,
   FieldConfig,
   DataLink,
